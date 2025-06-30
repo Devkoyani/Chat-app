3d81bbe9ff65ca199d0c25d8b216c94427df5758
devkoyani <devkoyani521@gmail.com>
2025-06-30 19:05:59 +0530

    Fix port and image

--- server/index.js
+++ server/index.js
@@ -55,7 +55,13 @@ app.use("/api/messages", messageRouter);
 await connectDB();
 
 // Start the server
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
+
+if(process.env.NODE_ENV === "production") {
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
+}
+
+// Export server for Vercel
+export default server;
